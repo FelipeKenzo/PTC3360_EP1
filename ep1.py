094ac96ffd137f2adc55b3def13492aa752da395
FelipeKenzo <felipe.kenzo@live.com>
2019-09-06 16:35:59 -0300

    First working example, removed progress bar.

--- ep1.py
+++ ep1.py
@@ -1,15 +1,23 @@
-import os, sys, argparse, subprocess, re, platform
-import time
+import os, sys, argparse, re, subprocess
+import matplotlib.pyplot as plt
+import numpy as np
 from tqdm import tqdm
 
 def EstimatedRTT(prevEstRTT, sampleRTT, alpha):
-	return alpha*prevEstRTT + (1-alpha)*sampleRTT
+	return (1-alpha)*prevEstRTT + alpha*sampleRTT
+
+def DevRTT(prevDevRTT, sampleRTT, estimatedRTT, beta):
+    return (1-beta)*prevDevRTT + beta*abs(sampleRTT-estimatedRTT)
+
+def toFloat(n):
+    return float(n)
 
 def main():
-    PING = ["ping", "google.com", "-c", "1"] # List of shell commands for subprocess.run
-    COUNT = 500	# number of SampleRTT values
-    ALPHA = 0.9 # EstimatedRTT coefficient
-    PATTERN = '(?<=time=)\d{1,3}\.\d{1,3}' # regex for parsing system response to ping command.
+    alpha = 0.125 # EstimatedRTT coefficient
+    beta = 0.25 # DevRTT coefficient
+    count = 500	# number of SampleRTT values
+    endpoint = "google.com"
+    rePattern = '(?<=time=)\d{1,3}\.\d{1,3}'
 
     # Command line arguments parsing
     parser = argparse.ArgumentParser(description = "Simple RTT analysis.")
@@ -23,38 +31,67 @@ def main():
     args = parser.parse_args()
 
     if args.endpoint is not None:
-        PING[1] = args.endpoint
+        endpoint = args.endpoint
     
     if args.count is not None and args.count >= 1:
-        COUNT = args.count
+        count = args.count
     
+    cmd = ["ping", endpoint, "-c", str(count)]
+
+
     if args.ipv4:
-        PING.append("-4")
+        cmd.append("-4")
     elif args.ipv6:
-        PING.append("-6")
+        cmd.append("-6")
 
-    # Data lists initialization
-    sampleRTT = [0]*COUNT
-    estimatedRTT = [0]*COUNT
-
-    # Acquire SampleRTT series.
-    for i in tqdm(range(COUNT)):
-        response = subprocess.run(PING, stdout = subprocess.PIPE)
-        currentRTT = re.findall(PATTERN, response.stdout.decode("utf-8"))
-        print
-        if currentRTT.count != 0:
-        	sampleRTT[i] = currentRTT[0]
-        else:
-        	i -= 1
+    # Acquire SampleRTT raw data.
+    print("Acquiring data for " + str(count) + " pings. This may take a while.")
     
-    print (sampleRTT)
+    sampleRTT = []
+    while True:
+        proc = subprocess.run(cmd, encoding="utf-8", stdout=subprocess.PIPE)
+        rawRTT = proc.stdout
 
+        # Parse raw SampleRTT data to float list (ms)
+        temp = re.findall(rePattern, rawRTT)
+        sampleRTT += list(map(toFloat, temp))
+
+        remaining = count - len(sampleRTT)
+        
+        print("Run completed, " + str(remaining) + " timeouts.")
+        
+        if remaining <= 0:
+            break
+
+        cmd[3] = str(remaining)
+
+    # Calculate EstimatedRTT and DevRTT lists
+    print("Calculating EstimatedRTT and DevRTT.")
+    estimatedRTT = [0]*count
+    devRTT = [0]*count
+    
     estimatedRTT[0] = sampleRTT[0]
+    devRTT[0] = 0.0
 
-    for i in range(1, estimatedRTT.count-1):
-    	estimatedRTT[i] = EstimatedRTT(estimatedRTT[i-1], sampleRTT[i], ALPHA)
+    for i in range(1, count):
+        print(i)
+        estimatedRTT[i] = EstimatedRTT(estimatedRTT[i-1], sampleRTT[i], alpha)
+        devRTT[i] = DevRTT(devRTT[i-1], sampleRTT[i], estimatedRTT[i], beta)
 
-    print (estimatedRTT)
+    print(estimatedRTT)
+    print(devRTT)
+
+    # Plotting
+
+    x = [0]*count
+    for i in range(count):
+        x[i] = i
+
+    plt.plot(x, sampleRTT, "r-", label="SampleRTT")
+    plt.plot(x, estimatedRTT, "g-", label="EstimatedRTT")
+    plt.plot(x, devRTT, "b-", label="DevRTT")
+    plt.legend(loc="best")
+    plt.show()
 
 
 if __name__ == "__main__":
